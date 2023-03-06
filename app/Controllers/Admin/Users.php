6222c87fb9010fdef0fcb97cb08efdc2c0be7f5d
bara1869 <aldikaronari@gmail.com>
2023-03-07 00:16:37 +0700

    Boostrap

---
+++ app/Controllers/Admin/Users.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\controllers\Admin;
+
+use App\Controllers\BaseController;
+
+class Users extends BaseController  
+{
+    public function index()
+    {
+        echo "ini adalah halaman Users";
+    }
+}
